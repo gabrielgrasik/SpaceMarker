d7b28d2188c114913395325bc8daad3fb472c765
gabrielgrasik <gabrielgrasik@gmail.com>
2024-06-24 10:51:46 -0300

    v.2

--- cmc.py
+++ cmc.py
@@ -1,1 +1,11 @@
+# Gabriel Grasik da Rosa    RA: 1136052 
 import pygame
+import tkinter as tk
+from tkinter import simpledialog
+import json
+import os
+
+pygame.init
+
+tela = pygame.display.set_mode(1000,563)
+
